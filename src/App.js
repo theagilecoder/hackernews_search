98567101caade63aa9df445311ffdce7929a3289
Pushkar Singh <theagilecoder@gmail.com>
2018-11-11 10:26:24 +0530

    Integrate HN API

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
+const DEFAULT_QUERY = "redux";
+
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   state = {
-    list: [
-      {
-        title: "React",
-        url: "https://reactjs.org/",
-        author: "Jordan Walke",
-        num_comments: 3,
-        points: 4,
-        objectID: 0
-      },
-      {
-        title: "Redux",
-        url: "https://redux.js.org/",
-        author: "Dan Abramov, Andrew Clark",
-        num_comments: 2,
-        points: 5,
-        objectID: 1
-      }
-    ],
-    searchTerm: ""
+    result: null,
+    searchTerm: DEFAULT_QUERY
   };
 
   onDismiss = id => {
-    const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updatedList });
+    const updatedHits = this.state.result.hits.filter(
+      item => item.objectID !== id
+    );
+    this.setState({ result: { ...this.state.result, hits: updatedHits } });
   };
 
   onSearchChange = event => {
     this.setState({ searchTerm: event.target.value });
   };
 
+  // Can remove this later...
+  setSearchTopStories = result => {
+    this.setState({ result });
+  };
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
   render() {
-    const { list, searchTerm } = this.state;
+    const { searchTerm, result } = this.state;
 
     return (
       <div className="page">
@@ -46,7 +49,13 @@ class App extends Component {
             Search{" "}
           </Search>
         </div>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        {result && (
+          <Table
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        )}
       </div>
     );
   }
